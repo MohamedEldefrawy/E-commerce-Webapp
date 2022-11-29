3cb22a2ede55ab0703fc16293889c8e84f267a7a
mohammed <mohammedre4a@gmail.com>
2022-11-30 00:18:38 +0200

    feat: add CRUD functions to UserService, UserRepository and UserController

--- src/main/java/com/vodafone/controller/UserController.java
+++ src/main/java/com/vodafone/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.vodafone.controller;
 
+import com.vodafone.model.User;
 import com.vodafone.model.dto.CreateUser;
 import com.vodafone.service.UserService;
 import lombok.AllArgsConstructor;
@@ -10,14 +11,37 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/users")
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserController {
     UserService userService;
+
     @GetMapping("login.htm")
-    public String login(String email, String password){
+    public String login(String email, String password) {
+        return null;
+    }
+
+    public String create(User user) {
+        return null;
+    }
+
+    public String update(Long id, User updatedEntity) {
+        return null;
+    }
+
+    public String delete(Long id) {
+        return null;
+    }
+
+    public String get(Long id) {
+        return null;
+    }
+
+    public String getAll() {
         return null;
     }
 }
