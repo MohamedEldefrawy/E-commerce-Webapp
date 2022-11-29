040efb1092dfb473279f4049f2cc4592beea0478
mohammed <mohammedre4a@gmail.com>
2022-11-29 16:25:07 +0200

    feat: add login function in UserController and UserService + wire controller with service.

--- src/main/java/com/vodafone/service/UserService.java
+++ src/main/java/com/vodafone/service/UserService.java
@@ -1,4 +1,16 @@
 package com.vodafone.service;
 
+import com.vodafone.repository.user.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserService {
+    UserRepository userRepository;
+    public String login(String email, String password) {
+        return userRepository.login(email,password);
+    }
 }
