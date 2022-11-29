c88d48587f712d69a9c4618c7982f269e1579761
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-11-29 14:45:14 +0200

    Add generic abstract CRUD methods to Repository interface

---
+++ src/main/java/com/vodafone/model/Product.java
@@ -0,0 +1,2 @@
+package com.vodafone.model;public class Product {
+}
--- src/main/java/com/vodafone/repository/Repository.java
+++ src/main/java/com/vodafone/repository/Repository.java
@@ -1,4 +1,15 @@
 package com.vodafone.repository;
 
+import java.util.List;
+
 public interface Repository<T> {
+    boolean create(T entity);
+
+    boolean update(T entity);
+
+    boolean delete(Long id);
+
+    T get(Long id);
+
+    List<T> getAll();
 }
