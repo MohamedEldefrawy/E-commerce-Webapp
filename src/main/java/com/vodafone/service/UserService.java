661fa4f0c4d1005f13d7d5f6fc3daa91fba127c7
mohammed <mohammedre4a@gmail.com>
2022-11-29 18:13:27 +0200

    refactor: replace repository type to match the interface [DI]

--- src/main/java/com/vodafone/service/UserService.java
+++ src/main/java/com/vodafone/service/UserService.java
@@ -1,6 +1,6 @@
 package com.vodafone.service;
 
-import com.vodafone.repository.user.UserRepository;
+import com.vodafone.repository.user.IUserRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserService {
-    UserRepository userRepository;
+    IUserRepository userRepository;
     public String login(String email, String password) {
         return userRepository.login(email,password);
     }
