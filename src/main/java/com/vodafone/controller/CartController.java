7963e89580b1b90d44b2fe6e6fe7dcfd56ef3c68
mohammed <mohammedre4a@gmail.com>
2022-11-30 01:02:48 +0200

    feat: define CRUD operations for Cart. feat: complete most of CartRepository class. refactor: change return data type for CartRepository and CartService functions to not return string.

--- src/main/java/com/vodafone/controller/CartController.java
+++ src/main/java/com/vodafone/controller/CartController.java
@@ -1,10 +1,16 @@
 package com.vodafone.controller;
 
+import com.vodafone.model.Cart;
 import com.vodafone.model.CartItem;
 import com.vodafone.service.CartService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -30,6 +36,30 @@ public class CartController {
     }
 
     public String getCartItems(Long cartId) {
+        return null;
+    }
+
+    public String createCart(Cart entity) {
+        return null;
+    }
+
+
+    public String updateCart(Long id, Cart updatedEntity) {
+        return null;
+    }
+
+
+    public String deleteCart(Long id) {
+        return null;
+    }
+
+
+    public String getCart(Long id) {
+        return null;
+
+    }
+
+    public String getAllCarts() {
         return null;
     }
 }
--- src/main/java/com/vodafone/repository/cart/CartRepository.java
+++ src/main/java/com/vodafone/repository/cart/CartRepository.java
@@ -2,13 +2,21 @@ package com.vodafone.repository.cart;
 
 import com.vodafone.config.HibernateConfig;
 import com.vodafone.model.Cart;
+import com.vodafone.model.CartItem;
+import com.vodafone.model.Order;
+import com.vodafone.model.OrderItem;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Repository
 @NoArgsConstructor
@@ -16,28 +24,152 @@ import java.util.ArrayList;
 public class CartRepository implements ICartRepository {
     HibernateConfig config;
 
+
     @Override
-    public String removeItem(Long cartId, Long itemId) {
-        return null;
+    public boolean create(Cart entity) {
+        try (Session session = config.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     @Override
-    public String clearCart(Long cartId) {
-        return null;
+    public boolean update(Long id, Cart updatedEntity) {
+        try (Session session = config.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            updatedEntity.setId(id);
+            session.update(updatedEntity);
+            transaction.commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     @Override
-    public String submitFinalOrder(Long cartId) {
-        return null;
+    public boolean delete(Long id) {
+        try (Session session = config.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Cart cart = get(id);
+            if (cart == null)
+                return false;
+            session.delete(cart);
+            transaction.commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     @Override
-    public String addItem(Long cartId, CartItem item) {
-        return null;
+    public Cart get(Long id) {
+        Cart cart = null;
+        try (Session session = config.getSessionFactory().openSession()) {
+            cart = session.get(Cart.class, id);
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return cart;
     }
 
     @Override
-    public String getCartItems(Long cartId) {
-        return null;
+    public List<Cart> getAll() {
+        List<Cart> carts = null;
+        try (Session session = config.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            carts = session.createQuery("SELECT DISTINCT cart From Cart cart", Cart.class).getResultList();
+            transaction.commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return carts;
+        }
+        return carts;
+    }
+
+    @Override
+    public boolean removeItem(Long cartId, Long itemId) {
+        try (Session session = config.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Cart cart = get(cartId);
+            cart.getItems().removeIf(item -> Objects.equals(item.getId(), itemId)); //remove desired item
+            session.update(cart); //update cart
+            transaction.commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean clearCart(Long cartId) {
+        try (Session session = config.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Cart cart = get(cartId);
+            //check if it's null so we can't operate
+            if (cart == null)
+                return false;
+            cart.getItems().clear();
+            session.delete(cart);
+            transaction.commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public Order submitFinalOrder(Long cartId) {
+        List<OrderItem> orderItems = new ArrayList<>();
+        Cart cart = get(cartId);
+        //iterate over each cart item to transform it to order item.
+        for (CartItem cartItem : cart.getItems()) {
+            OrderItem orderItem = new OrderItem();
+            //add product from cartItem to orderItem
+            //add quantity
+            //Todo: complete transformation validation -> waiting for Miand to add setters for OrderItem
+        }
+        Order order = new Order();
+        order.setDate(Date.valueOf(LocalDate.now()));
+        order.setCustomer(cart.getCustomer());
+        //todo: set orderItems list to Order
+        return order;
+    }
+
+    @Override
+    public boolean addItem(Long cartId, CartItem item) {
+        try (Session session = config.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Cart cart = get(cartId);
+            cart.getItems().add(item); //add item to cart list
+            session.update(cart); //update cart
+            transaction.commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public List<CartItem> getCartItems(Long cartId) {
+        return get(cartId).getItems();
     }
 }
