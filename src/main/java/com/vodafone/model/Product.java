04ef4abf66ad79a4bc309bad95142bb95d52faaa
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-11-29 14:45:29 +0200

    Create Product Entity

--- src/main/java/com/vodafone/model/Product.java
+++ src/main/java/com/vodafone/model/Product.java
@@ -1,2 +1,23 @@
-package com.vodafone.model;public class Product {
+package com.vodafone.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@ToString
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private String image;
+    private String category;
+    private float rate;
+    private double price;
 }
