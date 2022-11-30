734881d26b3a316e6de7e6b146a57be0f3c381b1
mohammed <mohammedre4a@gmail.com>
2022-11-30 16:10:04 +0200

    feat: add name property to Product.java

--- src/main/java/com/vodafone/model/Product.java
+++ src/main/java/com/vodafone/model/Product.java
@@ -15,6 +15,8 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
+    private String name;
     private String description;
     private String image;
     private String category;
