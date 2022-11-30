a1305b55a69dd1bb1cfbb4d9b629978dd1df7eb9
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-11-30 12:05:40 +0200

    remove constructor from Admin

--- src/main/java/com/vodafone/model/Admin.java
+++ src/main/java/com/vodafone/model/Admin.java
@@ -1,15 +1,10 @@
 package com.vodafone.model;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "admins")
-public class Admin extends User{
+public class Admin extends User {
 
 }
