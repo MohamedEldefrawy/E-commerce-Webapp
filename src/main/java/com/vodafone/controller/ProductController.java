5a29029186f09301d6d00d83a6cd2bd4f803ca96
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-11-30 22:34:04 +0200

    add Create Item action and view

--- src/main/java/com/vodafone/controller/ProductController.java
+++ src/main/java/com/vodafone/controller/ProductController.java
@@ -1,13 +1,20 @@
 package com.vodafone.controller;
 
+import com.vodafone.model.Product;
 import com.vodafone.model.dto.CreateProduct;
 import com.vodafone.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/product")
@@ -20,6 +27,22 @@ public class ProductController {
     public String create(Model model) {
         model.addAttribute("product", new CreateProduct());
         return "createProduct";
+    }
+
+    @PostMapping("create.htm")
+    public String save(@Valid @ModelAttribute("product") CreateProduct product, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, Object> model = bindingResult.getModel();
+            return "createProduct";
+        }
+
+        Product newProduct = new Product();
+        newProduct.setDescription(product.getDescription());
+        newProduct.setCategory(product.getCategory());
+        newProduct.setImage(product.getImage());
+        newProduct.setPrice(product.getPrice());
+        this.productService.create(newProduct);
+        return "redirect:/product/show.htm";
     }
 
     @GetMapping("show.htm")
--- src/main/webapp/WEB-INF/pages/productsTable.jsp
+++ src/main/webapp/WEB-INF/pages/productsTable.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: voisDev
-  Date: 2022-11-30
-  Time: 6:22 p.m.
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib uri="http://www.rapid-framework.org.cn/rapid" prefix="rapid" %>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
