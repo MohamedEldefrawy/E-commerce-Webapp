e6194122b833f48a971299ca3a6e83cca63ee9e9
mohammed <mohammedre4a@gmail.com>
2022-12-01 13:06:16 +0200

    feat: add function to reset password, request for resetting password and activation email.

--- src/main/java/com/vodafone/repository/customer/ICustomerRepository.java
+++ src/main/java/com/vodafone/repository/customer/ICustomerRepository.java
@@ -1,6 +1,7 @@
 package com.vodafone.repository.customer;
 
 import com.vodafone.model.Customer;
+import com.vodafone.model.Email;
 import com.vodafone.model.Order;
 import com.vodafone.repository.Repository;
 
@@ -8,4 +9,6 @@ import java.util.List;
 
 public interface ICustomerRepository extends Repository<Customer> {
     boolean resetPassword(String email, String password);
+    Email requestResetPassword(String email);
+    Email sendActivationEmail(String email);
 }
