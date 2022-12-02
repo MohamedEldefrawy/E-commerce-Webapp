ea248dabb9e43a05cc6011815c2d5a9ac911d5cc
mohammed <mohammedre4a@gmail.com>
2022-12-02 08:29:31 +0200

    feat: add validations and make use of LoginDTO in login function in UserRepository and UserService.

--- src/main/java/com/vodafone/repository/user/UserRepository.java
+++ src/main/java/com/vodafone/repository/user/UserRepository.java
@@ -1,9 +1,9 @@
 package com.vodafone.repository.user;
 
 import com.vodafone.config.HibernateConfig;
-import com.vodafone.model.Cart;
-import com.vodafone.model.Role;
-import com.vodafone.model.User;
+import com.vodafone.model.*;
+import com.vodafone.model.dto.CreateUser;
+import com.vodafone.model.dto.LoginDTO;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.hibernate.HibernateException;
@@ -23,20 +23,67 @@ public class UserRepository implements IUserRepository {
     HibernateConfig config;
 
     @Override
-    public User login(String email, String password) {
+    public LoginDTO login(String email, String password) {
+        /*
+         * Make use of LoginDTO to determine login status and validation @ controller
+         * Customer:
+         *   - Determine if user is suspended -> LoginDTO(T/F, suspended)
+         *   - Determine if user is deactivated -> LoginDTO(T/F, deactivated)
+         *   - Determine if user is activated  -> LoginDTO(T/F, activated)
+         *   - Determine if user is Admin  -> LoginDTO(T/F, Admin)
+         *   --------------------------------------------------------
+         *   when to accept and redirect user to its home [AdminPanel, home]
+         *       1. LoginStatus: Admin, isCredentialsValid: true
+         *       2. LoginStatus: Activated, isCredentialsValid: true
+         *   --------------------------------------------------------
+         *   Errors and redirect:
+         *      1. LoginStatus: Admin, isCredentialsValid: false -> display 'incorrect username or password'
+         *      2. LoginStatus: Activated, isCredentialsValid: false -> display 'incorrect username or password'
+         *      3. LoginStatus: Suspended -> redirect to reset password view
+         *      4. LoginStatus: Deactivated -> redirect to activate account view
+         *      5. LoginStatus: Not_Registered -> redirect to register page
+         * */
         User user;
+        LoginDTO loginDTO = new LoginDTO();
         try (Session session = config.getSessionFactory().openSession()) {
             //add queries criteria
             HashMap<String, String> queries = new HashMap<>();
             queries.put("email", email);
-            queries.put("password", password);
             user = session.get(User.class, queries);
+            if (user != null) { //user's email exist
+                if (user.getRole() == Role.Admin) {
+                    //set login response to admin
+                    loginDTO.setResponse(LoginResponse.ADMIN);
+                } else if (user.getRole() == Role.Customer) {
+                    //check customer status and login credentials
+                    Customer customer = (Customer) user;
+                    //set status
+                    switch (customer.getUserStatus()) {
+                        case ACTIVATED:
+                            loginDTO.setResponse(LoginResponse.ACTIVATED);
+                            break;
+                        case DEACTIVATED:
+                            loginDTO.setResponse(LoginResponse.DEACTIVATED);
+                            break;
+                        case SUSPENDED:
+                            loginDTO.setResponse(LoginResponse.SUSPENDED);
+                            break;
+                    }
+                }
+                //check credentials -> if email and password exists then return true
+                queries.put("password", password);
+                boolean isCredentialValid = session.get(User.class, queries) != null;
+                System.out.println("Is user's credentials valid?: " + isCredentialValid);
+                loginDTO.setCredentialsValid(isCredentialValid);
+            } else { //user not registered -> redirect to register.jsp
+                return new LoginDTO(false, LoginResponse.NOT_REGISTERED);
+            }
             session.close();
         } catch (HibernateException e) {
-            e.printStackTrace(); //todo: fix exception message
+            e.printStackTrace();
             return null;
         }
-        return user;
+        return loginDTO;
     }
 
     @Override
