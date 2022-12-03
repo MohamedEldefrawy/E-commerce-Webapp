7dfc86c338edf653297ad82d2c29e78b26a80173
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2022-12-03 04:14:20 +0200

    add codeActivation column to customer

--- src/main/java/com/vodafone/model/Customer.java
+++ src/main/java/com/vodafone/model/Customer.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Entity
 @Table ( name = "customers")
 public class Customer extends User {
+    private String code;
     @OneToOne
     private Cart cart;
     @Column (nullable = false)
