9d0e7af14a32848be66f874109894242a6748b7f
miand1 <miandmostafa@gmail.com>
2022-12-04 20:58:12 +0200

    fix: fixed merge conflicts

--- src/main/java/com/vodafone/service/CustomerService.java
+++ src/main/java/com/vodafone/service/CustomerService.java
@@ -15,12 +15,13 @@ public class CustomerService {
 
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
-        /*Customer customer = new Customer(UserStatus.ACTIVATED);
+        Customer customer = new Customer();
         customer.setEmail("mi@gmail.com");
         customer.setUserName("mi");
         customer.setRole(Role.Customer);
         customer.setPassword("1234");
-        customerRepository.create(customer);*/
+        customer.setUserStatus(UserStatus.ACTIVATED);
+        customerRepository.create(customer);
     }
 
 
