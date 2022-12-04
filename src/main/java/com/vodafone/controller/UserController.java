406130982ceebfcc63fb9b3f82853da57188925d
mohammed <mohammedre4a@gmail.com>
2022-12-04 18:25:37 +0200

    refactor: clean user's login function.

--- src/main/java/com/vodafone/controller/UserController.java
+++ src/main/java/com/vodafone/controller/UserController.java
@@ -24,36 +24,41 @@ public class UserController {
     @GetMapping("login.htm")
     public String login(String email, String password, HttpSession session) {
         LoginDTO loginDTO = userService.login(email, password);
-        session.setAttribute("email",email);
-        session.setAttribute("password",password);
+        session.setAttribute("email", email);
+        session.setAttribute("password", password);
         if (loginDTO == null) //exception occurred
         {
             System.out.println("Error occurred while logging in....");
         } else {
-            session.setAttribute("id",loginDTO.getUserId());
-            Admin admin = (Admin) userService.get(loginDTO.getUserId());
-            if (loginDTO.getStatus() == UserStatus.ADMIN && admin.isFirstLogin()) { //checks first-login flag
-                //todo: send email with new generated password
-                adminService.setFirstLoginFlag(admin.getId()); //set flag to false
-                return "redirect:/setAdminPassword.htm";
-            } else if (loginDTO.getStatus() == UserStatus.ADMIN && loginDTO.isCredentialsValid()) {
-                return "redirect:/products.htm";
-            } else if (loginDTO.getStatus() == UserStatus.ACTIVATED && loginDTO.isCredentialsValid()) {
+            session.setAttribute("id", loginDTO.getUserId());
+            if (loginDTO.getStatus() == UserStatus.ADMIN) { //Admin-only logic
+                Admin admin = (Admin) userService.get(loginDTO.getUserId());
+                if (admin.isFirstLogin()) {
+                    //todo: send email with new generated password
+                    adminService.setFirstLoginFlag(admin.getId()); //set flag to false
+                    return "redirect:/setAdminPassword.htm";
+                }
+                if (loginDTO.isCredentialsValid()) {
+                    return "redirect:/products.htm";
+                }
+            } else if (loginDTO.getStatus() == UserStatus.ACTIVATED && loginDTO.isCredentialsValid()) { //valid credentials customer
                 return "redirect:/home.htm";
             }
-            //todo: put below code into switch
             if ((loginDTO.getStatus() == UserStatus.ADMIN || loginDTO.getStatus() == UserStatus.ACTIVATED) && !loginDTO.isCredentialsValid()) {
                 //todo: display 'incorrect email or password is entered' message
                 System.out.println("Wrong Credentials!");
-            } else if (loginDTO.getStatus() == UserStatus.SUSPENDED) {
-                return "redirect:/resetPassword.htm";
-            } else if (loginDTO.getStatus() == UserStatus.DEACTIVATED) {
-                return "redirect:/verify.htm";
-            } else if (loginDTO.getStatus() == UserStatus.NOT_REGISTERED) {
-                return "redirect:/registration.htm";
+            } else { //User only logic
+                switch (loginDTO.getStatus()) {
+                    case SUSPENDED:
+                        return "redirect:/resetPassword.htm";
+                    case DEACTIVATED:
+                        return "redirect:/verify.htm";
+                    case NOT_REGISTERED:
+                        return "redirect:/registration.htm";
+                }
             }
         }
-        return null;
+        return "login";
     }
 
     @PostMapping
