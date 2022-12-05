c1b58f179adb6d79aa9c37c641b07b245130b70a
mohammed <mohammedre4a@gmail.com>
2022-12-05 12:59:30 +0200

    refactor: remove login function from UserRepository and UserService.
    refactor: delete LoginDTO because it's not used anymore.

--- src/main/java/com/vodafone/repository/user/IUserRepository.java
+++ src/main/java/com/vodafone/repository/user/IUserRepository.java
@@ -1,10 +1,8 @@
 package com.vodafone.repository.user;
 
 import com.vodafone.model.User;
-import com.vodafone.model.dto.LoginDTO;
 
 public interface IUserRepository {
-    LoginDTO login(String email, String password);
 
     User getByEmail(String email);
 
--- src/main/java/com/vodafone/service/UserService.java
+++ src/main/java/com/vodafone/service/UserService.java
@@ -1,23 +1,14 @@
 package com.vodafone.service;
 
 import com.vodafone.model.User;
-import com.vodafone.model.dto.CreateUser;
-import com.vodafone.model.dto.LoginDTO;
 import com.vodafone.repository.user.IUserRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class UserService {
     private IUserRepository userRepository;
-
-    public LoginDTO login(String email, String password) {
-        return userRepository.login(email,password);
-    }
 
     public User getUserByEmail(String email){ return userRepository.getByEmail(email); }
     public boolean verifyUserCredentials(String email,String password){ return userRepository.verifyUserCredentials(email,password); }
