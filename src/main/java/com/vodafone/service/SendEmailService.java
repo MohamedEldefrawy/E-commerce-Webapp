af8f835e66c178845aec1d29ebdb2873154149f5
mohammed yasser <47453799+mohammedysamir@users.noreply.github.com>
2022-12-06 15:57:35 +0200

    Handle Otp expiration in SendEmailService (#75)
    * feat: encapsulate hashing service in separate class.
    
    * feat: use HashService functions to encrypt and decrypt passwords
    
    * feat: annotate HashService with @Service
    
    * feat: encrypt new admin's password and save password in session for sending email.
    feat: send email and redirect to setAdminPassword view
    
    * feat: add hashService to LoginValidator
    
    * feat: add todo to fix validation
    
    * refactor: remove unnecessary symbol from salt declaration.
    
    * refactor: remove size attribute from password field
    
    * refactor: validate email and password separately.
    
    * refactor: delete function validateUserCredentials function because of conflicting with hashing.
    
    * feat: add customer's password hashing capabilities.
    
    * refactor: redirect + send email in Suspended and Deactivated cases in login.
    
    * refactor: handle db exceptions in CustomerRepository
    
    * feat: add validation on sending email service for first-log in admin
    
    * refactor: manage otp resend and generation.
    feat: add validation to not redirect until email is sent
    
    * refactor: modify otp logic in SendEmailService to handle nullable otp

--- src/main/java/com/vodafone/service/SendEmailService.java
+++ src/main/java/com/vodafone/service/SendEmailService.java
@@ -1,5 +1,6 @@
 package com.vodafone.service;
 
+import com.vodafone.model.Customer;
 import com.vodafone.model.Email;
 import com.vodafone.model.EmailType;
 import com.vodafone.model.User;
@@ -18,8 +19,11 @@ public class SendEmailService {
     static String from = "t.m.n.t.ecommerce";
     HashService hashService;
 
-    public SendEmailService(HashService hashService) {
+    CustomerService customerService;
+
+    public SendEmailService(HashService hashService, CustomerService customerService) {
         this.hashService = hashService;
+        this.customerService = customerService;
     }
 
     //generate verification code
@@ -55,7 +59,16 @@ public class SendEmailService {
             Email emailObj = new Email();
             switch (emailType) {
                 case ACTIVATION:
-                    emailObj = sendActivationEmail(httpSession);
+                    Customer customer = customerService.getByMail(user.getEmail());
+                    String otp = customer.getCode();
+                    if (otp == null) //if otp is expired then generate new one
+                    {
+                        otp = getRandom();
+                        customer.setCode(otp);
+                        customerService.update(customer.getId(), customer);
+                        httpSession.setAttribute("verificationCode", otp);
+                    }
+                    emailObj = sendActivationEmail(httpSession, otp);
                     break;
                 case FORGET_PASSWORD:
                     emailObj = requestResetPassword(httpSession);
@@ -101,7 +114,7 @@ public class SendEmailService {
         return emailObj;
     }
 
-    public Email sendActivationEmail(HttpSession session) {
+    public Email sendActivationEmail(HttpSession session, String otp) {
         Email emailObj = new Email();
         emailObj.setSubject("Activate your email");
         emailObj.setTo((String) session.getAttribute("email"));
@@ -110,7 +123,7 @@ public class SendEmailService {
         emailObj.setBody("Dear customer," +
                 "\nWe are happy that you decided to use our service." +
                 "\nYou could use below code to verify your account." +
-                "\n" + session.getAttribute("verificationCode"));
+                "\n" + otp);
         return emailObj;
     }
 
