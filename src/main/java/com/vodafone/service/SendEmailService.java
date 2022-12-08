e2ec581c66feb510413064d24354a975b54270b9
mohammed <mohammedre4a@gmail.com>
2022-12-08 16:58:36 +0200

    refactor: encapsulate email setters into one function

--- src/main/java/com/vodafone/service/SendEmailService.java
+++ src/main/java/com/vodafone/service/SendEmailService.java
@@ -50,7 +50,7 @@ public class SendEmailService {
             properties.put("mail.smtp.port", "587");
             properties.put("mail.smtp.auth", "true");
             properties.put("mail.smtp.starttls.enable", "true");
-//            properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+            properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
             properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
             properties.setProperty("mail.transport.protocol", "smtp");
 
@@ -62,28 +62,7 @@ public class SendEmailService {
             };
 
             Session session = Session.getInstance(properties, auth);
-            Email emailObj = new Email();
-            switch (emailType) {
-                case ACTIVATION:
-                    Customer customer = customerService.getByMail(user.getEmail());
-                    String otp = customer.getCode();
-                    if (otp == null) //if otp is expired then generate new one
-                    {
-                        otp = getRandom();
-                        customer.setCode(otp);
-                        customerService.update(customer.getId(), customer);
-                        httpSession.setAttribute("verificationCode", otp);
-                    }
-                    emailObj = sendActivationEmail(httpSession, otp);
-                    break;
-                case FORGET_PASSWORD:
-                    emailObj = requestResetPassword(httpSession);
-                    break;
-
-                case SET_ADMIN_PASSWORD:
-                    emailObj = sendAdminResetMail(httpSession);
-                    break;
-            }
+            Email emailObj = identifyEmailType(emailType, httpSession);
             // creates a new e-mail message
             Message msg = new MimeMessage(session);
 
@@ -106,45 +85,51 @@ public class SendEmailService {
 
     }
 
-    public Email requestResetPassword(HttpSession session) {
+    public Email identifyEmailType(EmailType emailType, HttpSession session) {
         Email emailObj = new Email();
-        emailObj.setSubject("Password reset");
+        String subject = "", body = "";
         emailObj.setTo((String) session.getAttribute("email"));
         emailObj.setFrom(from);
-        emailObj.setBody("Dear customer," +
-                "\nForget your password?" +
-                "\nYou are suspended due to exceeding your login attempts [3]"+
-                "\nYour next login will let you reset your password." +
-                "\nWish you happy shopping experience." +
-                "\nRegards," +
-                "\nTMNT team.");
+        //set subject and body based on EmailType flag
+        switch (emailType) {
+            case ACTIVATION:
+                Customer customer = customerService.getByMail((String) session.getAttribute("email"));
+                String otp = customer.getCode();
+                if (otp == null) //if otp is expired then generate new one
+                {
+                    otp = getRandom();
+                    customer.setCode(otp);
+                    customerService.update(customer.getId(), customer);
+                    session.setAttribute("verificationCode", otp);
+                }
+                subject = "Activate your email";
+                body = "Dear customer,\n" +
+                        "We are happy that you decided to use our service.\n" +
+                        "You could use below code to verify your account.\n" +
+                        "" + otp;
+                break;
+            case FORGET_PASSWORD:
+                subject = "Password reset";
+                body = "Dear customer,\n" +
+                        "Forget your password?\n" +
+                        "You are suspended due to exceeding your login attempts [3]\n" +
+                        "Your next login will let you reset your password.\n" +
+                        "Wish you happy shopping experience.\n" +
+                        "Regards,\n" +
+                        "TMNT team.";
+                break;
+
+            case SET_ADMIN_PASSWORD:
+                subject = "Activate your email";
+                body = "Welcome to Admins' family\nWe have created your account. and you can use below password for first login:\n "
+                        + session.getAttribute("dec_password") +
+                        "\nHead to TMNT to set your password" +
+                        "\nRegards," +
+                        "\nTMNT super admin.";
+                break;
+        }
+        emailObj.setSubject(subject);
+        emailObj.setBody(body);
         return emailObj;
     }
-
-    public Email sendActivationEmail(HttpSession session, String otp) {
-        Email emailObj = new Email();
-        emailObj.setSubject("Activate your email");
-        emailObj.setTo((String) session.getAttribute("email"));
-        emailObj.setFrom(from);
-        emailObj.setBody("Dear customer," +
-                "\nWe are happy that you decided to use our service." +
-                "\nYou could use below code to verify your account." +
-                "\n" + otp);
-        return emailObj;
-    }
-
-    public Email sendAdminResetMail(HttpSession session) {
-        Email emailObj = new Email();
-        emailObj.setSubject("Activate your email");
-        emailObj.setTo((String) session.getAttribute("newAdminEmail"));
-        emailObj.setFrom(from);
-        emailObj.setBody("Welcome to Admins' family" +
-                "\nWe have created your account. and you can use below password for first login:" +
-                "\n " + session.getAttribute("dec_password") +
-                "\nHead to TMNT to set your password" +
-                "\nRegards," +
-                "\nTMNT super admin.");
-        return emailObj;
-    }
-
 }
