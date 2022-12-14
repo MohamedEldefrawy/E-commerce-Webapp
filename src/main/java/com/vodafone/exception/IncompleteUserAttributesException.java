e07f99219c35752d8734723fd12dbbdf040e983d
mohammed <mohammedre4a@gmail.com>
2022-12-14 14:47:25 +0200

    feat: define new exceptions for Customer

---
+++ src/main/java/com/vodafone/exception/IncompleteUserAttributesException.java
@@ -0,0 +1,7 @@
+package com.vodafone.exception;
+
+public class IncompleteUserAttributesException extends RuntimeException{
+    public IncompleteUserAttributesException(String message){
+        super(message);
+    }
+}
