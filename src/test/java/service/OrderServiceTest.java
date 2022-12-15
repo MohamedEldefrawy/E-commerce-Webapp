4926bf4b7fb07755ab99883e0d4ba3d2365901f9
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2022-12-15 16:41:59 +0200

    add update method unit tests

--- src/test/java/service/OrderServiceTest.java
+++ src/test/java/service/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package service;
 
+import com.vodafone.exception.NullIdException;
 import com.vodafone.exception.NullOrderException;
 import com.vodafone.model.Customer;
 import com.vodafone.model.Order;
@@ -61,6 +62,110 @@ class OrderServiceTest {
         when(orderRepositoryMock.create(entity)).thenReturn(Optional.of(any(Long.class)));
         //Assert
         assertTrue(orderService.create(entity));
+    }
+
+    @Test
+    void update_NullOrder_rightId_returnException(){
+        ///Arrange
+        Customer customer = new Customer();
+        customer.setUserName("Neimat");
+        customer.setEmail("neimat.soliman.ismail@gmail.com");
+
+        OrderItem orderItem = new OrderItem();
+        orderItem.setProduct(new Product());
+        orderItem.setQuantity(6);
+
+        Set<OrderItem> orderItems = new HashSet<>();
+        orderItems.add(orderItem);
+
+        Order oldOrder =new Order();
+        Order updateOrder = null;
+        oldOrder.setCustomer(customer);
+        oldOrder.setOrderItems(orderItems);
+        //Act
+        when(orderRepositoryMock.update(1L,updateOrder)).thenThrow(new NullOrderException("Null Order Provided"));
+        //Assert
+        assertThrows(NullOrderException.class,()->orderService.update(1L,updateOrder));
+    }
+
+    @Test
+    void update_nullId_rightOrder__returnFalse(){
+        ///Arrange
+        Customer customer = new Customer();
+        customer.setUserName("Neimat");
+        customer.setEmail("neimat.soliman.ismail@gmail.com");
+
+        OrderItem orderItem = new OrderItem();
+        orderItem.setProduct(new Product());
+        orderItem.setQuantity(6);
+
+        Set<OrderItem> orderItems = new HashSet<>();
+        orderItems.add(orderItem);
+
+        Order oldOrder =new Order();
+        oldOrder.setCustomer(customer);
+        oldOrder.setOrderItems(orderItems);
+
+
+        Customer updatedCustomer = new Customer();
+        updatedCustomer.setUserName("Nei");
+        updatedCustomer.setEmail("neimat.soliman.ismail@gmail.com");
+        Order updateOrder = oldOrder;
+        updateOrder.setCustomer(updatedCustomer);
+        //Act
+        when(orderRepositoryMock.update(null,updateOrder)).thenThrow(new NullIdException("Null order id is provided"));
+        //Assert
+        assertThrows(NullIdException.class,()->orderService.update(null,updateOrder));
+    }
+
+    @Test
+    void update_rightOrder_rightId_returnTrue(){
+        ///Arrange
+        Customer customer = new Customer();
+        customer.setUserName("Neimat");
+        customer.setEmail("neimat.soliman.ismail@gmail.com");
+
+        OrderItem orderItem = new OrderItem();
+        orderItem.setProduct(new Product());
+        orderItem.setQuantity(6);
+
+        Set<OrderItem> orderItems = new HashSet<>();
+        orderItems.add(orderItem);
+
+        Order order =new Order();
+        order.setCustomer(customer);
+        order.setOrderItems(orderItems);
+
+        //Act
+        when(orderRepositoryMock.update(1L,order)).thenReturn(true);
+        //Assert
+        assertTrue(orderService.update(1L,order));
+
+    }
+
+    @Test
+    void update_rightOrder_wrongId_returnTrue(){
+        ///Arrange
+        Customer customer = new Customer();
+        customer.setUserName("Neimat");
+        customer.setEmail("neimat.soliman.ismail@gmail.com");
+
+        OrderItem orderItem = new OrderItem();
+        orderItem.setProduct(new Product());
+        orderItem.setQuantity(6);
+
+        Set<OrderItem> orderItems = new HashSet<>();
+        orderItems.add(orderItem);
+
+        Order order =new Order();
+        order.setCustomer(customer);
+        order.setOrderItems(orderItems);
+
+        //Act
+        when(orderRepositoryMock.update(1L,order)).thenReturn(true);
+        //Assert
+        assertFalse(orderService.update(2L,order));
+
     }
 
 
