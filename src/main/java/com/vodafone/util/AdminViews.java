46ebcf88b8aa7787bd974e33012e3ab2588539e9
miand1 <miandmostafa@gmail.com>
2022-12-16 20:22:07 +0200

    feat: adds missing pages

--- src/main/java/com/vodafone/util/AdminViews.java
+++ src/main/java/com/vodafone/util/AdminViews.java
@@ -8,6 +8,11 @@ public class AdminViews {
     public static final String ADMIN_UPDATE_PRODUCT = "products/update";
     public static final String ADMIN_CREATE_PRODUCT = "products/create";
     public static final String ADMIN_SHOW_PRODUCT_REDIRECT = "redirect:/admins/products/show.htm";
+    public static final String UPDATE_ADMIN = "admin/updateAdmin";
+    public static final String CREATE_ADMIN = "admin/createAdmin";
+    public static final String ALL_ADMINS_REDIRECT = "redirect:/admins/admins.htm";
+    public static final String ADMIN_RESET_PASSWORD = "admin/setAdminPassword";
+
 
 
     private AdminViews() {
