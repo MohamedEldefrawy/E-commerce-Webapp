ba88570e5619b03919931d81f012698221216f74
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-12-20 01:47:37 +0200

    Fix UserSericeUnitTest syntax errors

--- src/main/java/com/vodafone/repository/user/IUserRepository.java
+++ src/main/java/com/vodafone/repository/user/IUserRepository.java
@@ -1,8 +1,8 @@
 package com.vodafone.repository.user;
 
 import com.vodafone.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IUserRepository {
-
-    User getByEmail(String email);
+public interface IUserRepository extends JpaRepository<User, Long> {
+    User findFirstByEmail(String email);
 }
