cb4b49ea2a0f6604f4c64d3e743564064fad1444
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2022-12-20 03:52:15 +0200

    Refactor: extend CrudRepository

--- src/main/java/com/vodafone/repository/order/IOrderRepository.java
+++ src/main/java/com/vodafone/repository/order/IOrderRepository.java
@@ -1,12 +1,14 @@
 package com.vodafone.repository.order;
 
 import com.vodafone.model.Order;
-import com.vodafone.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface IOrderRepository extends Repository<Long, Order> {
+public interface IOrderRepository extends CrudRepository<Order,Long> {
+    Optional<List<Order>> findByCustomerId(Long customerId);
 
-    List<Order> getByCustomerId(Long customerId);
+//    List<Order> getByCustomerId(Long customerId);
 
 }
