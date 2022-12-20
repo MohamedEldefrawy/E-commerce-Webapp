4a941b637d07c8147b67cce2463f5e205651a3e7
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2022-12-20 06:04:40 +0200

    Refactor code

--- src/main/java/com/vodafone/service/OrderService.java
+++ src/main/java/com/vodafone/service/OrderService.java
@@ -2,8 +2,11 @@ package com.vodafone.service;
 
 import com.vodafone.exception.NullIdException;
 import com.vodafone.exception.NullOrderException;
+import com.vodafone.exception.customer.CustomerNotFoundException;
+import com.vodafone.exception.order.OrderFailedCreationException;
+import com.vodafone.exception.order.OrderNotFoundException;
 import com.vodafone.model.Order;
-import com.vodafone.repository.order.OrderRepository;
+import com.vodafone.repository.order.IOrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,42 +16,52 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class OrderService{
-    private OrderRepository orderRepository;
+    private IOrderRepository orderRepository;
 
-    public List<Order> getAll(){
-        return orderRepository.getAll().get();
+    public List<Order> getAllOrders(){
+        return (List<Order>) orderRepository.findAll();
     }
     public Order get(Long orderId){
-        if (orderId == null || !orderRepository.getById(orderId).isPresent() )
-            throw new NullIdException("Null order id is provided");
-        return orderRepository.getById(orderId).get();
+        return orderRepository.findById(orderId)
+                .orElseThrow(()-> new OrderNotFoundException("Couldn't find order "+orderId));
     }
-    public boolean create(Order order){
+    public Order createOrder(Order order){
         if(order ==null){
             throw new NullOrderException("Null Order Provided");
         }
         else if (order.getOrderItems() != null && !order.getOrderItems().isEmpty()){
-            return orderRepository.create(order).isPresent();
+            return orderRepository.save(order);
         }else {
-            return false;
+            throw new OrderFailedCreationException("Failed to create this order");
         }
     }
-    public boolean update(Long orderId,Order order){
+    public Order updateOrder(Long orderId,Order order){
         if (order == null)
             throw  new NullOrderException("Your updated Order is null and that's not provided");
         if (orderId == null )
             throw new NullIdException("Null order id is provided");
-        return orderRepository.update(orderId,order);
+
+        Order oldOrder = orderRepository.findById(orderId)
+                .orElseThrow(()->new OrderNotFoundException("Couldn't find this order "+orderId));
+
+        oldOrder.setOrderItems(order.getOrderItems());
+        oldOrder.setCustomer(order.getCustomer());
+        oldOrder.setDate(order.getDate());
+        oldOrder.setTotal(order.getTotal());
+
+        return orderRepository.save(oldOrder);
     }
-    public boolean delete(Long orderId){
+    public void deleteOrder(Long orderId){
         if (orderId == null )
             throw new NullIdException("Null order id is provided");
-        return orderRepository.delete(orderId);
+        orderRepository.deleteById(orderId);
     }
+
     public List<Order> getByCustomerId(Long customerId) {
         if (customerId == null )
             throw new NullIdException("Null Customer id is provided");
-        return orderRepository.getByCustomerId(customerId);
+        return orderRepository.findAllByCustomerId(customerId)
+                .orElseThrow(()-> new CustomerNotFoundException("Couldn't find orders for this customer "+customerId));
     }
 
 }
