4c1a635a6500d7137eef2618e23cf89b2259a3b3
miand1 <miandmostafa@gmail.com>
2022-12-20 12:48:15 +0200

    fix: changed admin Repo, added spring data

--- src/main/java/com/vodafone/repository/admin/IAdminRepository.java
+++ src/main/java/com/vodafone/repository/admin/IAdminRepository.java
@@ -1,14 +1,13 @@
 package com.vodafone.repository.admin;
 
 import com.vodafone.model.Admin;
-import com.vodafone.repository.Repository;
 import java.util.Optional;
 
+import org.springframework.data.repository.CrudRepository;
 
-public interface IAdminRepository extends Repository<Long, Admin> {
-    boolean setFirstLoginFlag(Long id);
+public interface IAdminRepository extends CrudRepository<Admin,Long> {
+    Optional<Admin> findAdminByEmailIgnoreCase(String email);
 
-    Optional<Admin> getByEmail(String email);
+    Optional<Admin> findAdminByUserNameIgnoreCase(String email);
 
-    Optional<Admin> getByUsername(String username);
 }
